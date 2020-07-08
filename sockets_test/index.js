ce19fe29f5a6ebd4507a64a39935a85009f0586b
mmach900 <m.mach.90@gmail.com>
2020-07-08 17:33:06 +0200

    no message

---
+++ sockets_test/index.js
@@ -0,0 +1,36 @@
+var socket = require('socket.io-client');
+
+socket = socket('http://localhost:3000/your-namespace', { transports: ['websocket'], query: { source_id: 'user_id', auth: 'auth' } });
+
+
+
+socket.on('connect', function () {
+    console.log('connected')
+    setTimeout(function () {
+        socket.emit('join',3);
+    }, 5000);
+});
+socket.on('#user_id', function (data) {
+    setInterval(function () {
+        socket.emit('time', new Date);
+    }, 5000);
+    console.log(data)
+});
+
+socket.on(`#conva_id`, function (msg) {
+
+})
+socket.on(`#user_id#invite`, function (msg) {
+
+})
+socket.on(`#devices`, function (msg) {
+
+})
+socket.on(`message-all`, function (msg) {
+
+    console.log(msg)
+
+})
+
+
+socket.on('disconnect', function () { console.log('disconnected') });
---
+++ src/index2.js
@@ -0,0 +1,50 @@
+// Module dependencies.
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const redis = require('socket.io-redis');
+const app = module.exports = express();
+app.set('port', process.env.PORT || 4000);
+
+const server = app.listen(app.get('port'), () => {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+const io_s = require('socket.io')(server);
+io_s.adapter(redis({ host: 'redis-13184.c78.eu-west-1-2.ec2.cloud.redislabs.com', port: 13184, auth_pass: "oERk63TBgqihQWMYHakRnyCQoi0MSni7" }));
+
+
+const io = io_s.of('/your-namespace');
+
+// view engine setup
+
+
+
+
+io.on('connection', (socket) => {
+
+    socket.on('message-all', (data) => {
+        console.log('message')
+       
+        io.emit('message-all', data);
+    });
+
+    socket.on('join', (room) => {
+        console.log('join')
+       // socket.join(room);
+       
+        io.emit('message-all', "Socket " + socket.id + " joined to room " + room);
+    });
+
+    socket.on('message-room', (data) => {
+        const room = data.room;
+        const message = data.message;
+        io.to(room).emit('message-room', data);
+    });
+
+
+});
+
+app.get('/clients', (req, res, next) => {
+    res.send(Object.keys(io.connected));
+});
