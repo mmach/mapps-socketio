cac13aee22cce46b26ff94a34edeb27d92f4cb1f
RXS\mmach-c <mmach-c@rxsinfo.com>
2022-10-17 19:12:46 +0200

    no message

--- src/http_request.js
+++ src/http_request.js
@@ -1,12 +1,13 @@
 
 const axios = require('axios')
 const { QueryList } = require('justshare-shared')
+const  stringify  = require('flatted').stringify;
 
 var http_request = async (action, model, lang, token, proj) => {
    return await axios({
         method: 'get',
-        url: process.env.URL + '/query?action=' + encodeURIComponent(JSON.stringify({
-            "action": action, "model": encodeURIComponent(JSON.stringify(
+        url: process.env.URL + '/query?action=' + encodeURIComponent(stringify({
+            "action": action, "model": encodeURIComponent(stringify(
                 model
             ))
         })),
